2e5de0fb33ec27758b6356447aad1a3f80e2b01b
Nicole Paz <nicole@Nicoles-MacBook-Pro.local>
2013-11-04 11:09:03 -0800

    forgot to populate the form if the user didn't complete the form

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -17,13 +17,12 @@ end
 post '/events/create' do
   @event = Event.create(organizer_name: params[:organizer_name], organizer_email: params[:organizer_email], title: params[:title], date: params[:date])
   unless @event.errors.empty?
-    # @event.errors.clear
-    # p @event.errors
-    # flash[:error] = @event.errors[:messages]
+    session[:name] = @event
     @error = @event.errors.full_messages
     p @error
     erb :event_add
   else
+    session[:name] = nil
     redirect '/'
   end
 end
